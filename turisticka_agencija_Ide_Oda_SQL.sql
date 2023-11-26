e9ec1528a19d28eba5816402706ce80a1ac07b1f
Roman <149431633+Rokiart@users.noreply.github.com>
2023-11-26 09:51:14 +0100

    Update turisticka_agencija_Ide_Oda_SQL.sql

--- turisticka_agencija_Ide_Oda_SQL.sql
+++ turisticka_agencija_Ide_Oda_SQL.sql
@@ -1,27 +1,18 @@
 use master;
 go
-drop database if exist Ide_Oda;
+drop database if exists Ide_Oda;
 go 
 create database Ide_Oda collate Croatian_CI_AS;
 go  
 use Ide_Oda;
 go
-creatae database korisnici(
 
-korisnik_id int not null primary key identity(1,1),
-ime varchar(50)not null ,
-prezime varchar(50)not null,
-adresa varchar(50),
-brojtelefona int not null,
-email varchar(50),
-oib char(11)
 
-);
 
 
 create table agencija(
 
-agencija_id not null primary key identity(1,1),
+agencija_id int not null primary key identity(1,1),
 naziv varchar(50) not null,
 adresa varchar(50),
 brojtelefona int not null,
@@ -34,17 +25,28 @@ informacije varchar (400)
 
 create table atrakcije(
 
-atrakcija_id not null primary key identity(1,1),
+atrakcija_id int not null primary key identity(1,1),
 naziv varchar(50)not null,
 vrstaatrakcije varchar(50) not null,
 mjestoatrakcije varchar(50) not null,
-cijena decimal(6,2) not null,
+cijena decimal(6,2) not null
 
-)
+);
+
+create table korisnici(
+korisnik_id int not null primary key identity(1,1),
+ime varchar(50) not null,
+prezime varchar(50) not null,
+brojtelefona int not null,
+email varchar(50),
+oib char(11) not null
+
+
+);
 
 create table vlasnici(
 
-vlasnik_id not null primary key identity(1,1),
+vlasnik_id int not null primary key identity(1,1),
 ime varchar(50)not null,
 prezime varchar(50) not null,
 brojtelefona int not null,
@@ -52,8 +54,38 @@ email varchar(50) not null,
 oib char(11) not null,
 iban varchar(50) not null
 
+<<<<<<< Updated upstream
+=======
 );
 
+create table smjestaji(
+smjestaj_id int not null primary key identity(1,1),
+ulica varchar(50) not null,
+kucnibroj int not null,
+drzava varchar(50) not null 
+ default('HRVATSKA') ,
+cijena decimal(6,2)
+
+>>>>>>> Stashed changes
+);
+
+
+create table rezervacije(
+rezervacija_id int not null primary key identity(1,1),
+smjestaj int references smjestaji(smjestaj_id),
+vrijemekorištenjasmjestaja datetime not null,
+atrakcija int null references atrakcije(atrakcija_id),
+vrijemekoristenjaatrakcije datetime null,
+korisnik int not null references korisnici(korisnik_id),
+
+);
+
+--create table racuni(
+--racun_id int not null primary key identity(1,1),
+--smjestaj int not null references smjestaji(cijena),
+--atrakcija int null references atrakcije(cijena),
+
+--);
 
 
 
