3bdd39f284abf573d5602c3ed4adacddab56ced4
Roman <149431633+Rokiart@users.noreply.github.com>
2023-12-02 10:46:26 +0100

    vježbe detaljnijeg pretraživanja

--- vježbanje pretraživanja.sql
+++ vježbanje pretraživanja.sql
@@ -1,4 +1,3 @@
-
 use svastara;
 
 select count(*) from kupci;
@@ -66,7 +65,14 @@ where postanskibroj like '31%';
 
 -- delete from mjesta where sifra=1945;
 
- update mjesta set postanskibroj='31431' where naziv = 'Beketinci';
+select * from mjesta where naziv='beketinci';
+
+-------------------------------------------------
+--delete from mjesta where ???????????????????
+------------------------------------------------------
+update mjesta set postanskibroj=null where naziv='Beketinci';
+
+update mjesta set postanskibroj='31431' where naziv = 'Beketinci';
 
  ------------------------------------------------------------
  -- Izlistaj imena i prezimena svih kupaca iz mjesta Livana
@@ -96,4 +102,70 @@ datum between '2016-01-01' and ' 2016-12-31';
 --zašto na kraju reda nema ";" ?
 select top 1 * from primke 
 
+select * from artikli;
+
+select cijena from artikli where cijena between 1000 and 1100;
+
+select kratkinaziv ,cijena from artikli 
+where cijena between 1000 and 1100;
+
+select count(*) from artikli 
+where cijena between 1000 and 1100;
+
+select sum (b.kolicina * b.cijena) as ukupniiznos
+from primke a
+inner join artiklinaprimci b on a.sifra=b.primka
+inner join artikli c on b.artikl=c.sifra
+where a.rednibroj='14778/2017';
+
+--iznos na svim primkama 2017-01
+select a.rednibroj , sum (b.kolicina * b.cijena) as ukupniiznos
+from primke a
+inner join artiklinaprimci b on a.sifra=b.primka
+inner join artikli c on b.artikl=c.sifra
+where a.datum between '2017-01-01' and '2017-01-31'
+group by a.rednibroj
+order by 2 desc;
+
+dovucen naziv dobavljaca
+
+select a.rednibroj ,d.naziv, 
+sum (b.kolicina * b.cijena) as ukupniiznos
+from primke a
+inner join artiklinaprimci b on a.sifra=b.primka
+inner join artikli c on b.artikl=c.sifra
+inner join dobavljaci d on a.dobavljac=d.sifra
+where a.datum between '2017-01-01' and '2017-01-31'
+group by a.rednibroj,d.naziv
+order by 3 desc;
+
+--naziv dobavljaca s cijenom vecom od 3000000
+
+select a.rednibroj ,d.naziv, 
+sum (b.kolicina * b.cijena) as ukupniiznos,
+avg (b.kolicina * b.cijena ) as prosjek,
+min (b.kolicina * b.cijena ) as minimalno,
+max (b.kolicina * b.cijena ) as maksimalno
+from primke a
+inner join artiklinaprimci b on a.sifra=b.primka
+inner join artikli c on b.artikl=c.sifra
+inner join dobavljaci d on a.dobavljac=d.sifra
+where a.datum between '2017-01-01' and '2017-12-31'
+group by a.rednibroj,d.naziv
+having sum(b.kolicina * b.cijena)>3000000
+order by 3 desc;
+
+--detaljno pretraživanje
+
+select count(*) from artikli;
+
+select distinct artikl from artiklinaprimci; --jedinstven artikl
+
+select * from artikli where sifra
+not in (select distinct artikl from artiklinaprimci); -- pod upit u zagradi
+
+-- brisanje 2 nepotrebnih artikala
+
+delete from artikli where sifra
+not in (select distinct artikl from artiklinaprimci);
 
