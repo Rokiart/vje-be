0909748a3d8a6841682a5af6a701fff15a466253
Roman <149431633+Rokiart@users.noreply.github.com>
2023-12-10 18:12:59 +0100

    scripta_katalog_slika

---
+++ katalogslika/SQLkatalogslika.sql
@@ -0,0 +1,35 @@
+use master;
+go
+drop database if exists katalogslika;
+go
+
+create database katalogslika;
+go
+alter database katalogslika collate Croatian_CI_AS;
+go
+use katalogslika;
+
+create table lokacije(
+    sifra int not null primary key identity(1,1),
+	ime varchar(50),
+	prezime varchar(50),
+	adresa varchar(50)
+	);
+create table velicine(
+    sifra int not null primary key identity(1,1),
+	velicinasaramom decimal(4,2),
+	velicinabezrama decimal(4,2)
+	);
+create table nazivi(
+    sifra int not null primary key identity(1,1),
+	naziv varchar(50) not null,
+	velicina int references velicine(sifra),
+	godina int,
+    dostupnost bit,
+	lokacija int references lokacije(sifra)
+	);
+create table slike(
+    sifra int not null primary key identity(1,1),
+	naziv int not null references nazivi(sifra),
+	);
+
